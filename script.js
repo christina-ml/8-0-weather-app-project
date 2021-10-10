ca784a96c17e276f7e3a3df22957c034ea070475
Christina Loiacono <loiaconochristina@gmail.com>
2021-10-09 20:28:44 -0400

    clears search bar after user input submitted. fixed indentation of code

--- script.js
+++ script.js
@@ -6,29 +6,31 @@ userInputForm.addEventListener("submit", (event)=>{
 
     /* Get information on user input, not button*/
     let userLocation = event.target["pick-location"].value;
-    // console.log("userLocation", userLocation); // test
+
+    /* Clears search bar after user input is submitted */
+    event.target["pick-location"].value = "";
 
     /* Fetch - information for url */
     fetch("https://wttr.in/" + userLocation + "?format=j1")
     .then((res)=>{
         return res.json();
     }).then((data)=>{
-            let city = data.nearest_area[0].areaName[0].value;
-            let region = data.nearest_area[0].region[0].value;
-            let country = data.nearest_area[0].country[0].value;
-            let currentWeather = data.current_condition[0].FeelsLikeF;
+        let city = data.nearest_area[0].areaName[0].value;
+        let region = data.nearest_area[0].region[0].value;
+        let country = data.nearest_area[0].country[0].value;
+        let currentWeather = data.current_condition[0].FeelsLikeF;
 
-            /* select main display section */
-            let weatherDisplay = document.querySelector(".display");
-            weatherDisplay.innerHTML = `
-            <h2>${city}</h2>
-                <ul id="current-city-weather">
-                    <li><b>Area:</b> ${city}</li>
-                    <li><b>Region:</b> ${region}</li>
-                    <li><b>Country:</b> ${country}</li>
-                    <li><b>Currently:</b> Feels Like ${currentWeather}˚F</li>
-                </ul>
-            `
+        /* select main display section */
+        let weatherDisplay = document.querySelector(".display");
+        weatherDisplay.innerHTML = `
+        <h2>${city}</h2>
+            <ul id="current-city-weather">
+                <li><b>Area:</b> ${city}</li>
+                <li><b>Region:</b> ${region}</li>
+                <li><b>Country:</b> ${country}</li>
+                <li><b>Currently:</b> Feels Like ${currentWeather}˚F</li>
+            </ul>
+        `
         }).catch((err)=>{
             console.log(err);
     })
